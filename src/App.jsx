d53cbe75d3f4e6dd96f45b1a338a397953462fff
ghsam-2408 <ghsam2408@gmail.com>
2025-12-30 14:58:10 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  
+      <div>
+        <h1 className='bg-red-400 font-bold flex justify-center'>Sam Junior`s Portifolio</h1>
+      </div>
+
+       
+    
+  )
+}
+
+export default App
