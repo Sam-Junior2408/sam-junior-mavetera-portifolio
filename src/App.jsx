afedac0f8548ee6ea32816aba341247892128c05
ghsam-2408 <ghsam2408@gmail.com>
2025-12-31 00:50:07 +0200

    added the navbar and marquee components to app

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './index.css'
+import Marquee from './components/layout/Marquee'
+import Navbar from './components/layout/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
   
-      <div>
-        <h1 className='bg-red-400 font-bold flex justify-center'>Sam Junior`s Portifolio</h1>
+      <div className='bg-[#0B0F19] text-gray-200 min-h-screen'>
+        {/* <h1 className='bg-red-400 font-bold flex justify-center'>Sam Junior`s Portifolio</h1> */}
+        <Marquee/>
+        <Navbar/>
       </div>
-
-       
-    
   )
 }
 
