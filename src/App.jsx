b48a9fc918766b38267ecf1d1a62680c638209d9
ghsam-2408 <ghsam2408@gmail.com>
2025-12-31 13:40:01 +0200

    registered the components in the App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import viteLogo from '/vite.svg'
 import './index.css'
 import Marquee from './components/layout/Marquee'
 import Navbar from './components/layout/Navbar'
+import Hero from './components/sections/Hero'
+import About from './components/sections/About'
+import Skills from './components/sections/Skills'
+import Projects from './components/sections/Projects'
+import Footer from './components/layout/Footer'
+import Contact from './components/sections/Contact'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +20,12 @@ function App() {
         {/* <h1 className='bg-red-400 font-bold flex justify-center'>Sam Junior`s Portifolio</h1> */}
         <Marquee/>
         <Navbar/>
+        <Hero/>
+        <About/>
+        <Skills/>
+        <Projects/>
+        <Contact/>
+        <Footer/>
       </div>
   )
 }
