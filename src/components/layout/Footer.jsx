9cd4235bc31b7ede1f40c5d15827da10e2bcdd01
ghsam-2408 <ghsam2408@gmail.com>
2025-12-31 13:39:11 +0200

    designed the footer

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='py-6 text-center text-gray-500 text-sm'>
+       &copy; 2025 Sam Junior Mavetera. Built with React & Tailwind.
+    </footer>
+  )
+}
+
+export default Footer
