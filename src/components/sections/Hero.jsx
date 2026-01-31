120f2e50659477b5746440ee4a651af4e809cc3d
ghsam-2408 <ghsam2408@gmail.com>
2026-01-31 12:20:48 +0200

    added download icon, and display properties

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import profile from '../../assets/profile.jpeg'
+import { Download } from 'lucide-react'
 
 const Hero = () => {
   return (
@@ -10,11 +11,15 @@ const Hero = () => {
                 <h1 className='text-5xl font-extrabold text-white leading-tight'>Sam Junior Mavetera</h1>
                 <p className='mt-4 text-xl text-cyan-400'>Full Stack Software Engineer</p>
                 <p className='mt-6 text-gray-400 max-w-xl'>I build secure, scalable, production-ready web applications using Spring Boot and React.</p>
-                <div className='mt-4'>
-                    <a href='#projects' className='px-6 mr-4 py-3 rounded-lg bg-cyan-500 text-black font-semibold'>View Projects</a>
-                    <a href='#contact' className='px-6 py-3 mr-4 rounded-lg border border-white/20 hover:border-cyan-400'>Contact Me</a>
-                    <a target='_blank' href='https://drive.google.com/file/d/1F2A7g7-IKRI9yDr3BMp9ogj7zuk_Nwh-/view?usp=drive_link' className='px-6 mr-4 py-3 rounded-lg bg-cyan-500 text-black font-semibold'>Resume</a>
+                <div className='mt-8 flex flex-wrap gap-4'>
+                    <a href='#projects' className='px-6 py-3 rounded-lg bg-cyan-500 text-black font-semibold hover:bg-cyan-600 transition'>View Projects</a>
 
+                    <a href='#contact' className='px-6 py-3 rounded-lg border border-cyan-400 text-cyan-400 font-semibold hover:bg-cyan-400 hover:text-black transition'>Contact Me</a>
+
+                    <a target='_blank' href='https://drive.google.com/file/d/1F2A7g7-IKRI9yDr3BMp9ogj7zuk_Nwh-/view?usp=drive_link' className='px-6 py-3 rounded-lg bg-cyan-500 text-black font-semibold hover:bg-cyan-600 transition flex items-center gap-2'>
+                        <Download size={20}/>
+                        Resume
+                    </a>
                 </div>
             </div>
             <div className='flex justify-center'>
